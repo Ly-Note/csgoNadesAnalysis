d9c81d80088cfd8ff6f8a8d2028497ddd598d250
luyi <451457347@qq.com>
2023-07-15 13:05:29 +0800

    完成数据库到View的功能，所有业务逻辑都大体完成

--- mysite/matches/urls.py
+++ mysite/matches/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 
-
+app_name = 'match'
 urlpatterns = [
-    path("", views.index, name="index"),
-    # as_view()
+    path("", views.SeriesView.as_view(), name="series"),
+    path("<str:seriesName>/", views.MatchesView.as_view(), name="matches"),
+    path("<str:seriesName>/<str:matchId>/", views.hurtsList, name="hurt"),
+    path("sucess", views.sucess, name="sucess"),
     path("uploads", views.upload_json, name="uploads"),
-    path("uploads_sucess", views.sucess, name="uploads_sucess"),
+    
 ]
